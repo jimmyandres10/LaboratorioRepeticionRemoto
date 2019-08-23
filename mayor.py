83e6a112a50079363b7ee9b4257d0a4c6f2c3440
jimmyandres10 <54144235+jimmyandres10@users.noreply.github.com>
2019-08-23 09:58:17 -0500

    ejercicio resuelto

--- mayor.py
+++ mayor.py
@@ -1,18 +1,29 @@
 def main():
-
-    print("escriba un numero para saber si es mayor que 0 ")
-    a = int(input("digite el valor de a "))
-
-    if a > 0 :
-        print("el numero es mayor a 0")
     
-    elif a == 0:
-        print("el numero es igual a 0")
+    print("escriba 10 numeros para definir cuantos son positivos, negativos y ceros")
+    
+    npositivo = 0
+    nnegativo = 0
+    ncero = 0
+    
+    
+    for i in range (1,11):
 
-    else:
-        print("el numero es menor a 0")
+        a = int(input(f"digite el {i} numero"))
+    
+        if a > 0 :
+            print(f"el numero {a} es mayor a 0")
+            npositivo = npositivo + 1
+    
+        elif a < 0:
+            print(f"el numero {a} es menor a 0")
+            nnegativo = nnegativo + 1
 
+        else:
+            print(f"el numero {a} es 0")
+            ncero = ncero + 1
 
+    print(f"La cantidad de numero positivos fue {npositivo}, la cantidad de numeros negativos fue {nnegativo}, la cantidad de 0 es {ncero}")
 
 if __name__ == "__main__":
     main()
