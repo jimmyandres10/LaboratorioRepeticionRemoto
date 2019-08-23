cafe53d55f0693d671fe9ae61d42a9ba20c05326
jimmyandres10 <54144235+jimmyandres10@users.noreply.github.com>
2019-08-23 11:45:04 -0500

    primo con while

--- primo.py
+++ primo.py
@@ -2,13 +2,16 @@
 print("escriba un numero para saber si es primo")
 a = int(input("de un numero "))
 contdep = 0
-for i in range(1,a//2):
+i = 1
+while i<=a//2 and contdep<=2:
+    i=i+1
     if a % i == 0:
         contdep = contdep + 1
 
 
-
-if contdep <= 1:
-    print(f"su numero es primo {a}")
+if contdep <= 1: 
+    print(f"el numero es primo {a} ")
 else:
-    print(f"su numero no es primo {a}")    
+    print(f"el numero no es primo {a} ")    
+         
+
