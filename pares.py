515a61fa5e8a0286e8c7b4baea58bb4272fc7470
jimmyandres10 <54144235+jimmyandres10@users.noreply.github.com>
2019-08-23 10:56:05 -0500

    Ejercicio dos resuelto

--- pares.py
+++ pares.py
@@ -1,18 +1,25 @@
-def main():
 
-    print("escriba el numero que desee saber si es par")
 
+print("algoritmo que lea una cantidad variable de números y determine cuántos números fueron pares y cuántos impares. El algoritmo dejará de leer números cuando digite un número cero. ")
+    
+ndeveces = int(input("escriba la cantidad de numeros que va a dar "))
+ndepares = 0
+ndeimpares = 0
+
+
+for i in range(1,ndeveces + 1 ):
+
+        
     a = int(input("de su numero "))
 
-    if a % 2 == 0 :
-        print(f"el numero {a} es par ")
-
+    if a==0 :
+        break
     else:
-        print(f"el numero {a} no es par " )
+        if a % 2 == 0 :
+            print(f"el numero {a} es par ")
+            ndepares = ndepares + 1
 
+        elif a % 2 != 0:
+            print(f"el numero {a} es impar " )
+            ndeimpares = ndeimpares + 1
 
-
-
-
-if __name__ == "__main__":
-    main()
